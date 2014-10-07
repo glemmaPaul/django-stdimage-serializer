e853100f4e17e622680fe2dbc009d8c76d2da198
Paul Oostenrijk <paul@glemma.nl>
2014-10-07 15:26:07 +0200

    fix(naming): Changed naming

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 
 setup(
     name='django-stdimage-serializer',
-    version='0.1.1',
+    version='0.1.2',
     author=u'Paul Oostenrijk',
     author_email='paul@glemma.nl',
-    packages=['django-stdimage-serializer'],
+    packages=['stdimage_serializer'],
     url='https://github.com/glemmaPaul/django-stdimage-serializer',
     license='Just dont bug me with your problems',
     description='A Serializing object for stdImage that shows all the variations in an object',
