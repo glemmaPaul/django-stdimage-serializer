b35c0540399f611ea148aa89fb368ad2ebca920a
Paul Oostenrijk <paul@glemma.nl>
2014-10-07 15:00:59 +0200

    Created a setup.py file that can install the package through pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='django-stdimage-serializer',
+    version='0.1.1',
+    author=u'Paul Oostenrijk',
+    author_email='paul@glemma.nl',
+    packages=['django-stdimage-serializer'],
+    url='https://github.com/glemmaPaul/django-stdimage-serializer',
+    license='Just dont bug me with your problems',
+    description='A Serializing object for stdImage that shows all the variations in an object',
+    zip_safe=False,
+)
